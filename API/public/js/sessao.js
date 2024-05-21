8b57303599db4ee1c17f997320de5b866f04b7a7
yEllenCaroline <ellen.chaves@sptech.school>
2024-05-20 21:27:46 -0300

    Progresso na criação do quiz

--- API/public/js/sessao.js
+++ API/public/js/sessao.js
@@ -1,36 +1,36 @@
-// sessão
-function validarSessao() {
-    var email = sessionStorage.EMAIL_USUARIO;
-    var nome = sessionStorage.NOME_USUARIO;
+// // sessão
+// function validarSessao() {
+//     var email = sessionStorage.EMAIL_USUARIO;
+//     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+//     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
-    } else {
-        window.location = "../login.html";
-    }
-}
+//     if (email != null && nome != null) {
+//         b_usuario.innerHTML = nome;
+//     } else {
+//         window.location = "../login.html";
+//     }
+// }
 
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../login.html";
-}
+// function limparSessao() {
+//     sessionStorage.clear();
+//     window.location = "../login.html";
+// }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+// // carregamento (loading)
+// // function aguardar() {
+// //     var divAguardar = document.getElementById("div_aguardar");
+// //     divAguardar.style.display = "flex";
+// // }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+// function finalizarAguardar(texto) {
+//     // var divAguardar = document.getElementById("div_aguardar");
+//     // divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.style.display = "flex";
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
 
